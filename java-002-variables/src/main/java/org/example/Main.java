5e99ebdb3ed0e74aac2b73fc152a5a1dec3d0b4c
Muhammed Yasir TUNA <76888043+yasirtuna@users.noreply.github.com>
2024-07-01 15:09:10 +0300

    Update Main.java

--- java-002-variables/src/main/java/org/example/Main.java
+++ java-002-variables/src/main/java/org/example/Main.java
@@ -3,6 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) { // bu bir method ( bir tane main methodu olmalı)
 
+        
+        System.out.println("Hello world!");
+
+        int sayi1 = 15;
+        int sayi2 = 35;
+        int sonuc = sayi1 + sayi2;
+
+        System.out.println("Sayi1: "+ sayi1);
+        System.out.println("Sayi2: "+ sayi2);
+        System.out.println("Sonuç: "+ sonuc);
+
+        System.out.println("Toplama: "+ (sayi1 + sayi2));
+        System.out.println("Çıkarma: " + (sayi2 - sayi1));
+        System.out.println("Bölme: "+ (sayi1 / sayi2));
+        System.out.println("Çarpma: "+ (sayi1 * sayi2));
 
     }
 }
