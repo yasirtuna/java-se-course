05e31e0b52a0fd14037280ae7121bcecb6fc584a
Muhammed Yasir TUNA <76888043+yasirtuna@users.noreply.github.com>
2024-07-01 15:08:46 +0300

    Update Car.java

--- java-002-variables/src/main/java/org/example/Car.java
+++ java-002-variables/src/main/java/org/example/Car.java
@@ -1,2 +1,67 @@
 package org.example;
+/*
+Birden
+fazla satır
+açıklaması
+*/
+public class Car {
+    
+    public static void main(String[] args) {
+        
+        
+         /*
+    Değişkenlerin isimlerinde
+    İngilizlerin albafebesindeki harfleri
+    kullanacağız.
+    */
 
+        // sayi teker
+       // int teker = 4;
+        byte teker = 4;
+
+        // sayi  direksiyon
+        int direksiyon = 1;
+
+
+        //   sayi motor gücü
+        int motorGucu = 5000;
+
+
+        // yazi motor türü
+        String motorTuru = "Alman";
+
+
+        //  yazi marka
+        String marka = "Mercedes";
+
+
+        //   sayi model yılı
+      //  int modelYili = 2024;
+        short modelYili = 2024;
+
+        //   yazi renk
+        String renk = "Gri";
+
+
+        //  var yok plaka
+        boolean plaka = true;
+
+
+        //sayi  kapi adeti
+        int kapiAdeti = 5;
+
+
+
+        System.out.println("---------------------------------------");
+        System.out.println("Araba sınıfı merhaba.");
+        System.out.println("Teker Sayısı: " + teker);
+        System.out.println("Direksiyon: " + direksiyon );
+        System.out.println("Motor Türü: " + motorTuru);
+        System.out.println("Plaka Durumu: " + plaka);
+        System.out.println("Kapı adeti: " + kapiAdeti);
+        System.out.println("---------------------------------------");
+    }
+
+
+//    Ülke.Şehir.İlçe.Köy.Sokak.Ev.No.Kat.Daire
+}
